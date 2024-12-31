66536d93f729d5e29b1533033ff68975d5ebc055
chokj <siregy72@gmail.com>
2024-12-31 16:53:57 +0900

    a

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+
+app.use("/", require("./routes/main"));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
