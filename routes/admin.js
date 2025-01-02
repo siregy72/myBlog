97f0115c263c447c4ec59dbe1135ab7fb1d2fb67
chokj <siregy72@gmail.com>
2025-01-02 16:23:12 +0900

    관리자 등록.

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const adminLayout = "../views/layouts/admin";
 const adminLayout2 = "../views/layouts/admin-nologout";
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 router.get("/admin", (req, res) => {
     const locals = {
@@ -9,5 +12,20 @@ router.get("/admin", (req, res) => {
     };
     res.render("admin/index", { locals, layout: adminLayout2 });
 });
+
+router.get("/register", asyncHandler( async (req, res) => { 
+    const locals = {
+        title: "Register"
+    };
+    res.render("admin/index", { locals, layout: adminLayout2 });
+}));
+
+router.post("/register", asyncHandler( async (req, res) => { 
+    const hassedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create({
+        username: req.body.username,
+        password: hassedPassword
+    });
+}));
 
 module.exports = router;
